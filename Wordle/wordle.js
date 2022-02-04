484034d9fc3f58538def18aa33c2ec391f024fa6
Benjamin Brennan <ben15jman@gmail.com>
2022-02-04 15:23:32 -0500

    new word

--- Wordle/wordle.js
+++ Wordle/wordle.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const hideLetters = document.getElementById("hide-icon")
     let guessedWords = [[]]
     let availableSpace = 1
-    let correctWord = "bugle"
+    let correctWord = "yacht"
     let guessedWordCount = 0
     let hardModeActivated = false
     createSquares(letters, guesses)
