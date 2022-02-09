79129486b28a06198e6a341a8132e8689da3fb1b
Benjamin Brennan <ben15jman@gmail.com>
2022-02-09 11:43:51 -0500

    changing word

--- Wordle/wordle.js
+++ Wordle/wordle.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const hideLetters = document.getElementById("hide-icon")
     let guessedWords = [[]]
     let availableSpace = 1
-    let correctWord = "geese"
+    let correctWord = "abyss"
     let guessedWordCount = 0
     let hardModeActivated = false
     
