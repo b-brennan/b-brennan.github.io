82650f90a491013cc796f68fc45ae3e3bc606801
Benjamin Brennan <ben15jman@gmail.com>
2022-02-09 23:02:20 -0500

    hopefully fixing keyboard overlap

--- Wordle/wordle.js
+++ Wordle/wordle.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const letters = 5
     const guesses = 6
-    const keys = document.querySelectorAll('.keyboard-row button')
+    const keys = document.querySelectorAll('.keyboard-container button')
     const toggle = document.getElementById("cbx")
     const dayIcon = document.getElementById("day")
     const nightIcon = document.getElementById("night")
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const hideLetters = document.getElementById("hide-icon")
     let guessedWords = [[]]
     let availableSpace = 1
-    let correctWord = "abyss"
+    let correctWord = "aloof"
     let guessedWordCount = 0
     let hardModeActivated = false
     
