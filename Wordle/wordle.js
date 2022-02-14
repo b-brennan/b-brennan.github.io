7215b831f96ab01eacf1e242d2848b7df2841d92
Benjamin Brennan <ben15jman@gmail.com>
2022-02-14 09:14:25 -0500

    changin word

--- Wordle/wordle.js
+++ Wordle/wordle.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const hideLetters = document.getElementById("hide-icon")
     let guessedWords = [[]]
     let availableSpace = 1
-    let correctWord = "glitz"
+    let correctWord = "heart"
     let guessedWordCount = 0
     let hardModeActivated = false
     
