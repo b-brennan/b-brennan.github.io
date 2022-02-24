4924b5f832a6e262a9f3a281a4114cc6510eff22
Benjamin Brennan <ben15jman@gmail.com>
2022-02-24 15:51:08 -0500

    fixing porfolio bugs

--- main.js
+++ main.js
@@ -4,25 +4,12 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
 
-// function openSlide(){
-//     slideMenu.style.width = "25%"
-//     slideMenu.style.marginLeft = "75%"
-
-// }
-
-// function closeSlide(){
-//     slideMenu.style.width = "0px"
-//     slideMenu.style.marginLeft = "100%"
-
-// }
-
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active")
     navMenu.classList.toggle("active")
 })
 
-document.querySelectorAll("nav-link").forEach(n => n.addEventListener("click", () => {
-    hamburger.clasList.remove("active")
-    navMenu.clasList.remove("active")
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.classList.remove("active")
+    navMenu.classList.remove("active")
 }))
-
