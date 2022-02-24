caa08cce54d9f736fd3bab013b15382f616b2d3a
Benjamin Brennan <ben15jman@gmail.com>
2022-02-24 15:09:08 -0500

    portfolio styling changes

--- main.js
+++ main.js
@@ -1,15 +1,28 @@
 // open and close slide menu
 
-let slideMenu = document.getElementById("menuSlide");
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
-function openSlide(){
-    slideMenu.style.width = "25%"
-    slideMenu.style.marginLeft = "75%"
 
-}
+// function openSlide(){
+//     slideMenu.style.width = "25%"
+//     slideMenu.style.marginLeft = "75%"
 
-function closeSlide(){
-    slideMenu.style.width = "0px"
-    slideMenu.style.marginLeft = "100%"
+// }
 
-}
+// function closeSlide(){
+//     slideMenu.style.width = "0px"
+//     slideMenu.style.marginLeft = "100%"
+
+// }
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active")
+    navMenu.classList.toggle("active")
+})
+
+document.querySelectorAll("nav-link").forEach(n => n.addEventListener("click", () => {
+    hamburger.clasList.remove("active")
+    navMenu.clasList.remove("active")
+}))
+
